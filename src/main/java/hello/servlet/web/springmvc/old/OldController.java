59a2f268222b1a8b856ed58a4b72b9c81b9eb521
moto4321 <moto432195@gmail.com>
2023-06-18 17:16:20 +0900

    ModelAndView 추가

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -12,6 +12,10 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+//        return null;
+        return new ModelAndView("new-form"); // 논리적인 이름만 넣은 상태, 뷰리졸버에서 물리적인 이름으로 바꿀 예정
+        // 그리고 resources/application.properties에서 다음을 추가
+        // spring.mvc.view.prefix=/WEB-INF/views/
+        // spring.mvc.view.suffix=.jsp
     }
 }
